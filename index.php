f59fa8b740795fe1b8eeb02ffe1981780851f7fb
Russell Bishop <hey@russellbishop.co.uk>
2019-11-11 23:39:29 +0000

    Colours and Type almost done

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-    $title = 'Prism Typography - Fluid Scales for Designers';
+    $title = 'DCS Web';
 
     $description = '';
 
@@ -10,25 +10,11 @@
 
 <body>
 
-<style id="prism-mqs" type="text/css">
-  /* */
-</style>
+<main class="">
 
-<main class="o-grid">
 
-  <?php include('components/views.php'); ?>
-  
-  <?php include('components/canvas.php'); ?>
 
-  <?php include('components/charts.php'); ?>
-  
 </main>
-
-<?php include('components/moustache-templates.php'); ?>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/3.0.2/mustache.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="/dist/js/index.js"></script>
 
 </body>
 </html>
