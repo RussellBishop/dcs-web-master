421e883d0502ba04b1c13bba7107f4635e09d8cf
Russell Bishop <hey@russellbishop.co.uk>
2019-11-25 14:02:24 +0000

    Colours, Type, Radius, Spacing…

--- index.php
+++ index.php
@@ -12,7 +12,29 @@
 
 <main class="">
 
+  <div class="c-tariff">
 
+    <h1 class="c-tariff__title">Gold Tariff</h1> 
+    <h2 class="c-tariff__description">For high-earners chargers</h2>
+
+    <dl class="u-mt-6">
+      <dt class="c-tariff__key">Activation</dt>
+      <dd class="c-tariff__value">€0,00</dd>
+      
+      <dt class="c-tariff__key">Monthly Fee</dt>
+      <dd class="c-tariff__value">€0,00</dd>
+    </dl>
+
+    <dl class="u-mt-6">
+      <h3 class="c-tariff__key">Charging</h3>
+      <p class="c-tariff__value">The charging rates for public charge points are set by the operator of the charge point.</p>
+      
+      <h3 class="c-tariff__key">Ionity</h3>
+      <p class="c-tariff__value">Exclusive membership and special prices for High Power Charging at Ionity Charging Stations.</p>
+    </dl>
+
+
+  </div>
 
 </main>
 
